647f40a3c49713ad4b2d3e27e489822273e02819
tezivindh <tezivindh@gmail.com>
2026-06-16 09:15:00 +0530

    feat: initialize Next.js project with TypeScript and Tailwind CSS

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ["*.trycloudflare.com"],
+};
+
+export default nextConfig;
+
